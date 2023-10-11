4533d6a6bd3680d24253701e46e1dc6db14fd0d6
tajultonim <mail@tajultonim.com>
2023-10-11 16:01:50 +0600

    ➕Add - Breaking table

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,3 @@
-const observeUrlChange = () => {
-  let oldHref = document.location.href;
-  const body = document.querySelector("body");
-  const observer = new MutationObserver((mutations) => {
-    if (oldHref !== document.location.href) {
-      oldHref = document.location.href;
-      if (document.location.href.includes("group-table")) {
-        observeAll();
-      }
-    }
-  });
-  observer.observe(body, { childList: true, subtree: true });
-};
-
 function observeAll() {
   var mutationObserver = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
@@ -21,6 +7,20 @@ function observeAll() {
           overlay.parentElement.remove();
         }
       }
+      console.log("obs");
+
+      let inpWrp = document.querySelector(".sc-cnOchM.bvArzR");
+      if (inpWrp) {
+        if (inpWrp.children.length == 2) {
+          inpWrp.removeChild(inpWrp.lastChild);
+          if (inpWrp.firstChild.disabled) {
+            inpWrp.firstChild.disabled = false;
+            inpWrp.firstChild.style.backgroundColor = "rgb(70, 77, 99)";
+            inpWrp.firstChild.style.color = "white";
+          }
+        }
+      }
+
       let ico = document.querySelectorAll(".sc-cXlbRe.eSzGXv");
       if (ico.length) {
         ico.forEach((i) => {
@@ -152,6 +152,16 @@ function observeAll() {
           nel.className = "sc-gvSjmx eSYusR c-emoji";
           nel.innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="sc-jmQrGM bSEXLr room-emoji-icon" color="#FFFFFF" height="24" width="24" xmlns="http://www.w3.org/2000/svg" style="color: rgb(255, 255, 255);"><path d="M288 421a48 48 0 1 0 96 0 48 48 0 1 0-96 0zm352 0a48 48 0 1 0 96 0 48 48 0 1 0-96 0zM512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm263 711c-34.2 34.2-74 61-118.3 79.8C611 874.2 562.3 884 512 884c-50.3 0-99-9.8-144.8-29.2A370.4 370.4 0 0 1 248.9 775c-34.2-34.2-61-74-79.8-118.3C149.8 611 140 562.3 140 512s9.8-99 29.2-144.8A370.4 370.4 0 0 1 249 248.9c34.2-34.2 74-61 118.3-79.8C413 149.8 461.7 140 512 140c50.3 0 99 9.8 144.8 29.2A370.4 370.4 0 0 1 775.1 249c34.2 34.2 61 74 79.8 118.3C874.2 413 884 461.7 884 512s-9.8 99-29.2 144.8A368.89 368.89 0 0 1 775 775zM664 533h-48.1c-4.2 0-7.8 3.2-8.1 7.4C604 589.9 562.5 629 512 629s-92.1-39.1-95.8-88.6c-.3-4.2-3.9-7.4-8.1-7.4H360a8 8 0 0 0-8 8.4c4.4 84.3 74.5 151.6 160 151.6s155.6-67.3 160-151.6a8 8 0 0 0-8-8.4z"></path></svg>`;
           ewrp.prepend(nel);
+          nel = document.createElement("div");
+          nel.onclick = function () {
+            if (confirm("সত্যি সত্যি ব্রেক করতে চান? 🥴") == true) {
+              breakTable();
+            }
+          };
+          nel.className = "sc-gvSjmx eSYusR";
+          nel.style.color = "white";
+          nel.innerHTML = `B`;
+          ewrp.prepend(nel);
         }
       }
 
@@ -227,28 +237,26 @@ function observeAll() {
 
   mutationObserver.observe(document.documentElement, {
     attributes: true,
-    characterData: true,
     childList: true,
     subtree: true,
-    attributeOldValue: true,
-    characterDataOldValue: true,
   });
 }
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.data) {
-    localStorage.setItem("roomId", message.data);
+    sessionStorage.setItem("roomId", message.data);
   }
 });
 
 function sendEmoji(emoji, b) {
   let token = localStorage.getItem("jwt");
-  let roomId = localStorage.getItem("roomId");
+  let roomId = sessionStorage.getItem("roomId");
   fetch(`https://elb.hilokal.com/group-call/${roomId}/emoji`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
+      Accept: "application/json",
     },
     body: JSON.stringify({
       emoji: emoji,
@@ -262,12 +270,13 @@ function sendEmoji(emoji, b) {
 
 function sendBean(n, txt, isan) {
   let token = localStorage.getItem("jwt");
-  let roomId = localStorage.getItem("roomId");
+  let roomId = sessionStorage.getItem("roomId");
   fetch(`https://elb.hilokal.com/group-call/${roomId}/messages`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
+      Accept: "application/json",
     },
     body: JSON.stringify({
       body: JSON.stringify({
@@ -281,6 +290,27 @@ function sendBean(n, txt, isan) {
     }),
   });
   toogleBean();
+}
+
+function breakTable() {
+  let token = localStorage.getItem("jwt");
+  let roomId = sessionStorage.getItem("roomId");
+  fetch(`https://elb.hilokal.com/group-call/${roomId}/messages`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      body: "🥴",
+      type: "bean",
+      parentId: null,
+      image: null,
+    }),
+  }).then(() => {
+    location.reload();
+  });
 }
 
 function toogleBean() {
@@ -297,4 +327,4 @@ function toggleModal() {
   }
 }
 
-window.onload = observeUrlChange;
+window.onload = observeAll;
